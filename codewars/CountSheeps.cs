10f72ea5703b7b04cbfdae9afec7d5dce23cfb90
Martin R-L <martin.rosenlidholm@gmail.com>
2018-12-03 12:37:33 +0100

    as-is from https://www.codewars.com/kata/counting-sheep-dot-dot-dot/train/csharp

---
+++ codewars/CountSheeps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace codewars
+{
+    public static class Kata
+    {
+        public static int CountSheeps(bool[] sheeps)
+        {
+            //TODO
+        }
+    }
+    
+    [TestFixture]
+    public class CountSheepsTests 
+    {
+        [Test]
+        public void SampleTest() 
+        {
+            var sheeps = new bool[] { true, false, true };
+    
+            Assert.AreEqual(2, Kata.CountSheeps(sheeps));
+        }
+    }
+}
