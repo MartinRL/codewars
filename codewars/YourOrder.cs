1e2412c380663b74689b6732994b952afcca37f4
MartinRL <martin.rosenlidholm@gmail.com>
2018-12-04 14:12:56 +0100

    boilerplate and theory examples from kata spec.

---
+++ codewars/YourOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class YourOrderKata
+    {
+        public static string Order(string words)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+    
+    public class YourOrderTests
+    {
+        [Theory]
+        [InlineData("is2 Thi1s T4est 3a", "Thi1s is2 3a T4est")]
+        [InlineData("4of Fo1r pe6ople g3ood th5e the2", "Fo1r the2 g3ood 4of th5e pe6ople")]
+        [InlineData("", "")]
+        public void RunOrderTheory(string s, string expected)
+        {
+            YourOrderKata.Order(s).Should().Be(expected);
+        }
+    }
+}
