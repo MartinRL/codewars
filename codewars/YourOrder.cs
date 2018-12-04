6aa5da57c2df4e03bd4a46106e9506b78d3a67a0
MartinRL <martin.rosenlidholm@gmail.com>
2018-12-04 14:21:51 +0100

    Revert "make one example pass"
    This reverts commit b591d23aa37d260120a6ef6e6da813a580167280.

--- codewars/YourOrder.cs
+++ codewars/YourOrder.cs
@@ -8,9 +8,6 @@ namespace codewars
     {
         public static string Order(string words)
         {
-            if (string.IsNullOrEmpty(words))
-                return string.Empty;
-            
             throw new NotImplementedException();
         }        
     }
@@ -21,9 +18,9 @@ namespace codewars
         [InlineData("is2 Thi1s T4est 3a", "Thi1s is2 3a T4est")]
         [InlineData("4of Fo1r pe6ople g3ood th5e the2", "Fo1r the2 g3ood 4of th5e pe6ople")]
         [InlineData("", "")]
-        public void RunOrderTheory(string words, string sortedWords)
+        public void RunOrderTheory(string s, string expected)
         {
-            YourOrderKata.Order(words).Should().Be(sortedWords);
+            YourOrderKata.Order(s).Should().Be(expected);
         }
     }
 }
