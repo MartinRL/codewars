c1283aad7ee802286a447da37fa1cedc8c021414
MartinRL <martin.rosenlidholm@gmail.com>
2018-12-04 14:23:11 +0100

    renames :lipstick:

--- codewars/YourOrder.cs
+++ codewars/YourOrder.cs
@@ -21,9 +21,9 @@ namespace codewars
         [InlineData("is2 Thi1s T4est 3a", "Thi1s is2 3a T4est")]
         [InlineData("4of Fo1r pe6ople g3ood th5e the2", "Fo1r the2 g3ood 4of th5e pe6ople")]
         [InlineData("", "")]
-        public void RunOrderTheory(string s, string expected)
+        public void RunOrderTheory(string words, string orderedWords)
         {
-            YourOrderKata.Order(s).Should().Be(expected);
+            YourOrderKata.Order(words).Should().Be(orderedWords);
         }
     }
 }
