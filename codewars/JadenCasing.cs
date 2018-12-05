f67050cab3b5ec76de7b1fbd4c727ba9f8b7239b
MartinRL <martin.rosenlidholm@gmail.com>
2018-12-05 15:10:43 +0100

    add example and implement kata

--- codewars/JadenCasing.cs
+++ codewars/JadenCasing.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using FluentAssertions;
 using Xunit;
 
@@ -8,7 +9,7 @@ namespace codewars
     {
         public static string JadenCase(string s)
         {
-            throw new NotImplementedException();
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s);
         }
     }
 
@@ -17,6 +18,7 @@ namespace codewars
 
         [Theory]
         [InlineData("How can mirrors be real if our eyes aren't real", "How Can Mirrors Be Real If Our Eyes Aren't Real")]
+        [InlineData("this is me", "This Is Me")]
         public void RunJadenCaseTheory(string s, string jadenCased)
         {
             JadenCasingKata.JadenCase(s).Should().Be(jadenCased);
