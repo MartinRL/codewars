77a730df29392feaea6d889877fac63f6241d01c
MartinRL <martin.rosenlidholm@gmail.com>
2018-12-05 10:54:38 +0100

    add boilerplate and theory example from spec.

---
+++ codewars/JadenCasing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public static class JadenCasingKata
+    {
+        public static string JadenCase(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class JadenCasingTests
+    {
+
+        [Theory]
+        [InlineData("How can mirrors be real if our eyes aren't real", "How Can Mirrors Be Real If Our Eyes Aren't Real")]
+        public void RunJadenCaseTheory(string s, string jadenCased)
+        {
+            JadenCasingKata.JadenCase(s).Should().Be(jadenCased);
+        }
+    }
+}
