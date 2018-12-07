458f0bc8ed29b9c2a0a8163f93bd41837437ea62
Martin R-L <martin.rosenlidholm@gmail.com>
2018-12-07 16:24:13 +0100

    :lipstick:

--- codewars/Scramblies.cs
+++ codewars/Scramblies.cs
@@ -9,10 +9,11 @@ namespace codewars
     {
         public static bool Scramble(string scrambled, string word)
         {
-            return word.ToCharArray().All(l => scrambled.ToCharArray().Contains(l)) ? scrambled.ToCharArray().Where(l => word.ToCharArray().Contains(l)).OrderBy(_ => _).GroupBy(_ => _)
-                .Zip(word.ToCharArray().OrderBy(_ => _).GroupBy(_ => _), (sg, wg) => sg.Count() >= wg.Count())
-                .All(_ => _) :
-                false;
+            return word.ToCharArray().All(l => scrambled.ToCharArray().Contains(l)) && 
+                   scrambled.ToCharArray()
+                       .Where(l => word.ToCharArray().Contains(l)).OrderBy(_ => _).GroupBy(_ => _)
+                       .Zip(word.ToCharArray().OrderBy(_ => _).GroupBy(_ => _), (sg, wg) => sg.Count() >= wg.Count())
+                       .All(_ => _);
         }
     }
 
