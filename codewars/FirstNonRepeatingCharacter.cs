aa0f58933856d3bdc851f5971af25218f0d8cbec
Martin R-L <martin.rosenlidholm@gmail.com>
2018-12-09 11:48:20 +0100

    make all examples pass

--- codewars/FirstNonRepeatingCharacter.cs
+++ codewars/FirstNonRepeatingCharacter.cs
@@ -12,7 +12,43 @@ namespace codewars
             if (string.IsNullOrWhiteSpace(s))
                 return string.Empty;
             
-            return s.GroupBy(_ => _).First(g => g.Count() == 1).Key.ToString();
+            return s.GroupBy(g => new Letter(g)).First(g => g.Count() == 1).Key.ToString();
+        }
+    }
+
+    public class Letter
+    {
+        private readonly char letter;
+
+        public Letter(char letter)
+        {
+            this.letter = letter;
+        }
+
+        protected bool Equals(Letter other)
+        {
+            return char.ToLower(letter) == char.ToLower(other.letter);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            
+            if (ReferenceEquals(this, obj))
+                return true;
+            
+            return obj.GetType() == GetType() && Equals((Letter) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return char.ToLower(letter).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return letter.ToString();
         }
     }
 
@@ -23,6 +59,7 @@ namespace codewars
         [InlineData("a", "a")]
         [InlineData("stress", "t")]
         [InlineData("moonmen", "e")]
+        [InlineData("SsT", "T")]
         public void ExecuteOrderExample(string s, string l)
         {
             FirstNonRepeatingCharacterKata.FirstNonRepeatingLetter(s).Should().Be(l);
