ee0b78c7c671a9d4516cd09e7e1836fd793024fd
Martin R-L <martin.rosenlidholm@gmail.com>
2018-12-17 09:07:10 +0100

    add boilerplate and a few theory examples
    https://www.codewars.com/kata/array-dot-diff/train/csharp

---
+++ codewars/ArrayDiff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public static class ArrayDiffKata
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ArrayDiffTests
+    {
+        [Theory]
+        [InlineData(new [] { 1, 2}, new [] { 1 }, new [] { 2 })]
+        [InlineData(new [] { 1, 2, 2}, new [] { 1 }, new [] { 2, 2 })]
+        [InlineData(new [] { 1, 2, 2}, new [] { 2 }, new [] { 1 })]
+        public void ElementsInBShouldBeRemovedFromA(int[] a, int[] b, int[] c)
+        {
+            ArrayDiffKata.ArrayDiff(a, b).Should().BeEquivalentTo(c);
+        }
+    }
+}
