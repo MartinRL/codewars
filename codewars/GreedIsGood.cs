8d885076dbb42c0706d94a1b2f9e6e825bebd8f2
Martin R-L <martin.rosenlidholm@gmail.com>
2018-12-18 21:47:22 +0100

    add boilerplate and examples from the spec.
    https://www.codewars.com/kata/greed-is-good/train/csharp

---
+++ codewars/GreedIsGood.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public static class GreedIsGoodKata
+    {
+        public static int Score(int[] dice) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GreedIsGoodTests
+    {
+        [Theory]
+        [InlineData(new[] {2, 3, 4, 6, 2}, 0)]
+        [InlineData(new[] {4, 4, 4, 3, 3}, 400)]
+        [InlineData(new[] {2, 4, 4, 5, 4}, 450)]
+        [InlineData(new[] {5, 1, 3, 4, 1}, 250)]
+        [InlineData(new[] {1, 1, 1, 3, 1}, 1100)]
+        public static void ShouldBeWorthless(int[] dice, int score)
+        {
+            GreedIsGoodKata.Score(dice).Should().Be(score);
+        }
+    }
+}
