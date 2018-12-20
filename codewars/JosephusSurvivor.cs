abc47e51eb311d1434ae62dcd5ba5320f2f42ddf
MartinRL <martin.rosenlidholm@gmail.com>
2018-12-20 12:28:06 +0100

    add boilerplate and examples from spec.
    https://www.codewars.com/kata/josephus-survivor/train/csharp

---
+++ codewars/JosephusSurvivor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public static class JosephusSurvivorKata
+    {
+        public static int JosSurvivor(int n, int k) 
+        {
+            throw new NotImplementedException();
+        }  
+    }
+
+    public class JosephusSurvivorTests
+    {
+        [Theory]
+        [InlineData(7, 3, 4)]
+        [InlineData(11, 19, 10)]
+        [InlineData(40, 3, 28)]
+        [InlineData(14, 2, 13)]
+        [InlineData(100, 1, 100)]
+        [InlineData(1, 300, 1)]
+        [InlineData(2, 300, 1)]
+        [InlineData(5, 300, 1)]
+        [InlineData(7, 300, 7)]
+        [InlineData(300, 300, 265)]
+        public void ExecuteJosSurvivorExample(int n, int k, int expected)
+        {
+            JosephusSurvivorKata.JosSurvivor(n, k).Should().Be(expected);
+        }
+    }
+}
