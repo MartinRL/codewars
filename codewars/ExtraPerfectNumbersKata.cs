13bd11380bcd378a91b080044d55311e1aadf4a8
MartinRL <martin.rosenlidholm@gmail.com>
2019-04-10 15:51:58 +0200

    make all tests :green_heart:

--- codewars/ExtraPerfectNumbersKata.cs
+++ codewars/ExtraPerfectNumbersKata.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using Xunit;
@@ -9,7 +10,17 @@ namespace codewars
     {
         public static int[] ExtraPerfect(int n)
         {
-            throw new NotImplementedException();
+            IList<int> perfects = new List<int>();
+
+            for (var i = 0; i <= n; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    perfects.Add(i);
+                }
+            }
+
+            return perfects.ToArray();
         }
     }
 
@@ -23,7 +34,7 @@ namespace codewars
         [InlineData(39, new [] {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39})]
         public void VerifyExtraPerfectWith(int n, int[] expected)
         {
-            ExtraPerfectNumbersSolution.ExtraPerfect(n).Should().AllBeEquivalentTo(expected);
+            ExtraPerfectNumbersSolution.ExtraPerfect(n).Should().BeEquivalentTo(expected);
         }
     }
 }
