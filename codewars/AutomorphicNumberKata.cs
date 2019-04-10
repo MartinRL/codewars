7750cc5320dd00fb0f467d1771ec207cd0cdba43
MartinRL <martin.rosenlidholm@gmail.com>
2019-04-10 12:39:37 +0200

    boilerplate and test cases from codewars
    https://www.codewars.com/kata/automorphic-number-special-numbers-series-number-6/train/csharp

---
+++ codewars/AutomorphicNumberKata.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class AutomorphicNumberSolution
+    {
+        public static string Automorphic(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AutomorphicNumberTests
+    {
+        [Theory]
+        [InlineData(1, "Automorphic")]
+        [InlineData(3, "Not!!")]
+        [InlineData(6, "Automorphic")]
+        [InlineData(9, "Not!!")]
+        [InlineData(25, "Automorphic")]
+        [InlineData(53, "Not!!")]
+        [InlineData(76, "Automorphic")]
+        [InlineData(95, "Not!!")]
+        [InlineData(625, "Automorphic")]
+        [InlineData(225, "Not!!")]
+        public void VerifyAutomorphicWith(int n, string expected)
+        {
+            AutomorphicNumberSolution.Automorphic(n).Should().Be(expected);
+        }
+    }
+}
