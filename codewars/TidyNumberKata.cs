dbfd30a1d8c763813f8b41a34dc2d72d2ae2e97e
MartinRL <martin.rosenlidholm@gmail.com>
2019-04-12 09:55:17 +0200

    boilerplate and tests from codewars

---
+++ codewars/TidyNumberKata.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class TidyNumberSolution
+    {
+        public static bool TidyNumber(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TidyNumberTests
+    {
+        [Theory]
+        [InlineData(12, true)]
+        [InlineData(2789, true)]
+        [InlineData(2335, true)]
+        [InlineData(102, false)]
+        [InlineData(9672, false)]
+        public void VerifyTidyNumberWith(int n, bool expected)
+        {
+            TidyNumberSolution.TidyNumber(n).Should().Be(expected);
+        }
+    }
+}
