ffa0f43bd118d0167acd0f595fa2f2d1b2e534cc
Martin Rosén-Lidholm <mrol@mvno.local>
2019-04-15 15:17:40 +0200

    add boilerplate and tests from codewars
    https://www.codewars.com/kata/students-final-grade/train/csharp

---
+++ codewars/StudentsFinalGradeKata.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class StudentsFinalGradeSolution
+    {
+        public static int FinalGrade(int exam, int projects)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StudentsFinalGradeTests
+    {
+        [Theory]
+        [InlineData(100, 12, 100)]
+        [InlineData(99, 0, 100)]
+        [InlineData(10, 15, 100)]
+        [InlineData(85, 5, 90)]
+        [InlineData(55, 3, 75)]
+        [InlineData(55, 0, 0)]
+        [InlineData(20, 2, 0)]
+        public void VerifyFinalGradeWith(int exam, int projects, int expected)
+        {
+            StudentsFinalGradeSolution.FinalGrade(exam, projects).Should().Be(expected);
+        }
+    }
+}
