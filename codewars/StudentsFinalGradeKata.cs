224f73e8a03b9552db1095c29bc3502427b1cfdf
Martin Rosén-Lidholm <mrol@mvno.local>
2019-04-15 15:33:24 +0200

    :lipstick: ?:

--- codewars/StudentsFinalGradeKata.cs
+++ codewars/StudentsFinalGradeKata.cs
@@ -9,16 +9,10 @@ namespace codewars
     {
         public static int FinalGrade(int exam, int projects)
         {
-            if (exam > 90 || projects > 10)
-                return 100;
-
-            if (exam > 75 && projects >= 5)
-                return 90;
-
-            if (exam > 50 && projects >= 2)
-                return 75;
-
-            return 0;
+            return exam > 90 || projects > 10 ? 100 :
+                   exam > 75 && projects > 4 ? 90 :
+                   exam > 50 && projects > 1 ? 75 :
+                   0;
         }
     }
 
