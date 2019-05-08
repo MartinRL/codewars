a607d6e135e96ad7c09fc0baa483ea43050cfb05
MartinRL <martin.rosenlidholm@gmail.com>
2019-05-08 12:53:49 +0200

    boilerplate and test cases from codewars
    https://www.codewars.com/kata/digital-cypher-vol-3-missing-key/train/csharp

---
+++ codewars/DigitalCypherVol3MissingKeyKata.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class DigitalCypherVol3MissingKeySolution
+    {
+        public static int FindTheKey(string message, int[] code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DigitalCypherVol3MissingKeyTests
+    {
+        [Theory]
+        [InlineData("scout", new[] { 20, 12, 18, 30, 21 } , 1939)]
+        [InlineData("masterpiece", new[] { 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8 } , 1939)]
+        [InlineData("nomoretears", new[] { 15, 17, 14, 17, 19, 7, 21, 7, 2, 20, 20 } , 12)]
+        public void VerifyFindTheKeyWith(string message, int[] code, int key)
+        {
+            DigitalCypherVol3MissingKeySolution.FindTheKey(message, code).Should().Be(key);
+        }
+    }
+}
