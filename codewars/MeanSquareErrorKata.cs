13e985382cdc0fdbd1177877241f36b9379e060e
Martin Rosén-Lidholm <mrol@mvno.local>
2019-05-20 12:38:57 +0200

    boilerplate and tests from codewars
    https://www.codewars.com/kata/mean-square-error/train/csharp

---
+++ codewars/MeanSquareErrorKata.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class MeanSquareErrorSolution
+    {
+        public static double Solution(int[] firstArray, int[] secondArray)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MeanSquareErrorTests
+    {
+        [Theory]
+        [InlineData(new[] {1, 2, 3}, new[] {4, 5, 6}, 9)]
+        [InlineData(new[] {10, 20, 10, 2}, new[] {10, 25, 5, -2}, 16.5)]
+        [InlineData(new[] {0, -1}, new[] {-1, 0}, 1)]
+        public void VerifySolutionWith(int[] firstArray, int[] secondArray, double expectedAverage)
+        {
+            MeanSquareErrorSolution.Solution(firstArray, secondArray).Should().Be(expectedAverage);
+        }
+    }
+}
