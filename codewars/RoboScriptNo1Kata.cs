8fec1ffab7a714b3d29c3a2c3f7d40d7dab8d9ea
Martin Rosén-Lidholm <mrol@mvno.local>
2019-05-24 12:19:28 +0200

    boilerplate and tests from codewars
    https://www.codewars.com/kata/roboscript-number-1-implement-syntax-highlighting/train/csharp

---
+++ codewars/RoboScriptNo1Kata.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class RoboScriptNo1Solution
+    {
+        public static string Highlight(string code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RoboScriptNo1Tests
+    {
+        [Theory]
+        [InlineData("F3RF5LF7", "<span style=\"color: pink\">F</span><span style=\"color: orange\">3</span><span style=\"color: green\">R</span><span style=\"color: pink\">F</span><span style=\"color: orange\">5</span><span style=\"color: red\">L</span><span style=\"color: pink\">F</span><span style=\"color: orange\">7</span>")]
+        [InlineData("FFFR345F2LL", "<span style=\"color: pink\">FFF</span><span style=\"color: green\">R</span><span style=\"color: orange\">345</span><span style=\"color: pink\">F</span><span style=\"color: orange\">2</span><span style=\"color: red\">LL</span>")]
+        public void VerifyHighlightWith(string code, string expectedHighlightedCode)
+        {
+            RoboScriptNo1Solution.Highlight(code).Should().Be(expectedHighlightedCode);
+        }
+    }
+}
