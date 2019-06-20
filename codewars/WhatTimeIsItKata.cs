8617ec2cdb71212cf66448e7a77fb238ddd68f16
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-06-20 11:15:03 +0200

    one test case green

--- codewars/WhatTimeIsItKata.cs
+++ codewars/WhatTimeIsItKata.cs
@@ -9,8 +9,19 @@ namespace codewars
         public static string GetMilitaryTimeFromStandardTime(string time)
         {
             // NB. Not allowed to use System.DateTime.
+
+            var period = time.Substring(time.Length - 2);
+            var hour = time.Substring(0, 2);
+
+            if (period == "AM")
+            {
+                if (hour == "12")
+                    throw new NotImplementedException();
+
+                return time.Substring(0, time.Length - period.Length);
+            }
             
-            throw new NotImplementedException();
+            throw new NotImplementedException($"time: {time}, period: {period}, hour: {hour}");
         }
     }
 
@@ -19,6 +30,7 @@ namespace codewars
         [Theory]
         [InlineData("12:00:01AM", "00:00:01")]
         [InlineData("11:46:47PM", "23:46:47")]
+        [InlineData("11:46:47AM", "11:46:47")]
         [InlineData("12:00:00AM", "00:00:00")]
         [InlineData("12:00:00PM", "12:00:00")]
         [InlineData("07:05:45PM", "19:05:45")]
