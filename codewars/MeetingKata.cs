ed34805032c347a30637dfaf853c6702789b989e
Martin Rosén-Lidholm <mrol@mvno.local>
2019-07-01 11:04:33 +0200

    boilerplate and tests from codewars
    https://www.codewars.com/kata/meeting/train/csharp

---
+++ codewars/MeetingKata.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class MeetingSolution
+    {
+        public static string FormatAndOrderAccordingToJohnsRequirements(string friends)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MeetingTests
+    {
+        [Theory]
+        [InlineData("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn",
+            "(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)")]
+        [InlineData("John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell",
+            "(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)")]
+        [InlineData("Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern",
+            "(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)")]
+        public void VerifyFormatAndOrderAccordingToJohnsRequirementsWith(string friends, string formattedAndOrderedFriends)
+        {
+            MeetingSolution.FormatAndOrderAccordingToJohnsRequirements(friends).Should().Be(formattedAndOrderedFriends);
+        }
+    }
+}
