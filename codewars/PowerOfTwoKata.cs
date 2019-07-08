84e0f915e5b42c13b38ed04da7eab4535716b652
Martin Rosén-Lidholm <mrol@mvno.local>
2019-07-08 07:46:21 +0200

    add egde case tests

--- codewars/PowerOfTwoKata.cs
+++ codewars/PowerOfTwoKata.cs
@@ -21,6 +21,8 @@ namespace codewars
         [InlineData(1024, true)]
         [InlineData(5, false)]
         [InlineData(333, false)]
+        [InlineData(1, true)]
+        [InlineData(0, false)]
         public void VerifyIsPowerOfTwqWith(int n, bool expected)
         {
             PowerOfTwoSolution.IsPowerOfTwo(n).Should().Be(expected);
