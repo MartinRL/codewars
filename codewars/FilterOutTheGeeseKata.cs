282140dbc034a2162f763d9947ede881319a7903
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-08-01 13:33:06 +0200

    boilerplate and tests from codewars
    https://www.codewars.com/kata/filter-out-the-geese/train/csharp

---
+++ codewars/FilterOutTheGeeseKata.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class FilterOutTheGeeseSolution
+    {
+        public static IEnumerable<string> GooseFilter(IEnumerable<string> birds)
+        {
+            var geese = new [] { "African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher" };
+            
+            return geese;
+        }
+    }
+    
+    public class FilterOutTheGeeseTests
+    {
+        [Theory]
+        [InlineData(new[] {"Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"},
+                    new[] {"Mallard", "Hook Bill", "Crested", "Blue Swedish"})]
+        [InlineData(new[] {"Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"}, 
+                    new[] {"Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"})]
+        [InlineData(new[] {"African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"}, 
+                    new string[] {})]
+        public void VerifyGooseFilter(IEnumerable<string> birds, IEnumerable<string> expectedBirdsWithoutGeese)
+        {
+            FilterOutTheGeeseSolution.GooseFilter(birds).Should().BeEquivalentTo(expectedBirdsWithoutGeese);
+        }
+    }
+}
