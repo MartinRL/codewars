9b481a43311c99cd91db5accddce781d59b40f1b
Martin Rosén-Lidholm <mrol@mvno.local>
2019-08-28 14:46:24 +0200

    add failing test from algo description
    https://www.geeksforgeeks.org/find-next-greater-number-set-digits/

--- codewars/NextBiggerNumberWithTheSameDigitsKata.cs
+++ codewars/NextBiggerNumberWithTheSameDigitsKata.cs
@@ -56,6 +56,7 @@ namespace codewars
         [InlineData(513, 531)]
         [InlineData(2017, 2071)]
         [InlineData(1234, 1243)]
+        [InlineData(534976, 536479)]
         [InlineData(9, -1)]
         [InlineData(111, -1)]
         [InlineData(531, -1)]
