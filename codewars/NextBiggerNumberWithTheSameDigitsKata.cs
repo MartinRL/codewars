d59d82b3a1d9dc5d4dc4bdfcfd34e35aab8a2366
Martin Rosén-Lidholm <mrol@mvno.local>
2019-08-29 14:56:48 +0200

    :lipstick: comment

--- codewars/NextBiggerNumberWithTheSameDigitsKata.cs
+++ codewars/NextBiggerNumberWithTheSameDigitsKata.cs
@@ -12,12 +12,15 @@ namespace codewars
         {
             var nAsCharArray = n.ToString().ToCharArray();
             
+            // If all digits sorted in descending order, then output is -1. For example, 4321
             if (nAsCharArray.OrderByDescending(_ => _).SequenceEqual(nAsCharArray))
                 return -1;
 
+            // If all digits are sorted in ascending order, then we need to swap last two digits. For example, 1234
             if (nAsCharArray.OrderBy(_ => _).SequenceEqual(nAsCharArray))
                 return long.Parse(new string(nAsCharArray.Swap(nAsCharArray.LastIndex(), nAsCharArray.SecondLastIndex())));
 
+            // I) Traverse the given number from rightmost digit, keep traversing till you find a digit which is smaller than the previously traversed digit. For example, if the input number is “534976”, we stop at 4 because 4 is smaller than next digit 9. If we do not find such a digit, then output is -1.
             var d = nAsCharArray[nAsCharArray.LastIndex()];
             var i = nAsCharArray.SecondLastIndex();
 
@@ -29,13 +32,16 @@ namespace codewars
 
             d = nAsCharArray[i];
 
+            // II) Now search the right side of above found digit ‘d’ for the smallest digit greater than ‘d’. For “534976″, the right side of 4 contains “976”. The smallest digit greater than 4 is 6.
             var subArrayToTheRightOfDigit = nAsCharArray.SubArray(i + 1);
 
             var smallestGreaterThanDigit = subArrayToTheRightOfDigit.Where(_ => _ > d).Min();
             var indexOfSmallestGraterThanDigit = Array.LastIndexOf(nAsCharArray, smallestGreaterThanDigit);
 
+            // III) Swap the above found two digits, we get 536974 in above example.
             var nWithDigitsSwapped = nAsCharArray.Swap(i, indexOfSmallestGraterThanDigit);
             
+            // IV) Now sort all digits from position next to ‘d’ to the end of number. The number that we get after sorting is the output. For above example, we sort digits in bold 536974. We get “536479” which is the next greater number for input 534976.
             var sortedRightOfDigit = nWithDigitsSwapped.SubArray(i + 1).OrderBy(_ => _);
 
             return long.Parse(new string(nWithDigitsSwapped
