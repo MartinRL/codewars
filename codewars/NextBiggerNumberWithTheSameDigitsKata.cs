b846f0903e8bcdeafd9d9e567d733cdad2172d2b
Martin Rosén-Lidholm <mrol@mvno.local>
2019-08-30 10:34:59 +0200

    :lipstick: simplify

--- codewars/NextBiggerNumberWithTheSameDigitsKata.cs
+++ codewars/NextBiggerNumberWithTheSameDigitsKata.cs
@@ -10,80 +10,21 @@ namespace codewars
     {
         public static long CalculateNextBiggerNumber(long n)
         {
-            var nAsCharArray = n.ToString().ToCharArray();
-            
-            // If all digits sorted in descending order, then output is -1. For example, 4321
-            if (nAsCharArray.OrderByDescending(_ => _).SequenceEqual(nAsCharArray))
-                return -1;
-
-            // If all digits are sorted in ascending order, then we need to swap last two digits. For example, 1234
-            if (nAsCharArray.OrderBy(_ => _).SequenceEqual(nAsCharArray) && nAsCharArray.Distinct().Count() == nAsCharArray.Length)
-                return long.Parse(new string(nAsCharArray.Swap(nAsCharArray.LastIndex(), nAsCharArray.SecondLastIndex())));
-
-            // I) Traverse the given number from rightmost digit, keep traversing till you find a digit which is smaller than the previously traversed digit. For example, if the input number is “534976”, we stop at 4 because 4 is smaller than next digit 9. If we do not find such a digit, then output is -1.
-            var d = nAsCharArray[nAsCharArray.LastIndex()];
-            var i = nAsCharArray.SecondLastIndex();
-
-            while (d <= nAsCharArray[i])
+            for (var i = n + 1; i <= long.Parse(n.AsDescendingString()); i++)
             {
-                d = nAsCharArray[i];
-                --i;
+                if(n.AsDescendingString() == i.AsDescendingString())
+                    return i;
             }
-
-            d = nAsCharArray[i];
-
-            // II) Now search the right side of above found digit ‘d’ for the smallest digit greater than ‘d’. For “534976″, the right side of 4 contains “976”. The smallest digit greater than 4 is 6.
-            var subArrayToTheRightOfDigit = nAsCharArray.SubArray(i + 1);
-
-            var smallestGreaterThanDigit = subArrayToTheRightOfDigit.Where(_ => _ > d).Min();
-            var indexOfSmallestGraterThanDigit = Array.LastIndexOf(nAsCharArray, smallestGreaterThanDigit);
-
-            // III) Swap the above found two digits, we get 536974 in above example.
-            var nWithDigitsSwapped = nAsCharArray.Swap(i, indexOfSmallestGraterThanDigit);
             
-            // IV) Now sort all digits from position next to ‘d’ to the end of number. The number that we get after sorting is the output. For above example, we sort digits in bold 536974. We get “536479” which is the next greater number for input 534976.
-            var sortedRightOfDigit = nWithDigitsSwapped.SubArray(i + 1).OrderBy(_ => _);
-
-            return long.Parse(new string(nWithDigitsSwapped
-                .SubArray(0, nWithDigitsSwapped.Length - sortedRightOfDigit.Count()).Concat(sortedRightOfDigit)
-                .ToArray()));
+            return -1;  
         }
     }
 
     public static class NextBiggerNumberWithTheSameDigitsExtensions
     {
-        public static T[] Swap<T>(this T[] @this, int firstIndex, int secondIndex)
+        public static string AsDescendingString(this long n)
         {
-            Swap(ref @this[firstIndex], ref @this[secondIndex]);
-
-            return @this;
-        }
-        
-        private static void Swap<T>(ref T a, ref T b)
-        {
-            var temp = a;
-            a = b;
-            b = temp;
-        }
-
-        public static int LastIndex<T>(this IEnumerable<T> @this)
-        {
-            return @this.Count() - 1;
-        }
-        
-        public static int SecondLastIndex<T>(this IEnumerable<T> @this)
-        {
-            return @this.LastIndex() - 1;
-        }
-        
-        public static T[] SubArray<T>(this T[] @this, int index, int length = -1)
-        {
-            var l = length == -1 ? @this.Length - index : length;
-            
-            var result = new T[l];
-            Array.Copy(@this, index, result, 0, l);
-            
-            return result;
+            return new string(n.ToString().OrderByDescending(_ => _).ToArray());
         }
     }
 
