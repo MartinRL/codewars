ce0a03661fbaf143ab79dd3cb079520729a4ebe5
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-09-24 13:54:22 +0200

    add edge case

--- codewars/NiceArrayKata.cs
+++ codewars/NiceArrayKata.cs
@@ -7,14 +7,15 @@ namespace codewars
 {
     public class NiceArraySolution
     {
-        public static bool IsNice(int[] arr) => arr.All(n => arr.Any(m => n - 1 == m || n + 1 == m));
+        public static bool IsNice(int[] arr) => arr.Length != 0 && arr.All(n => arr.Any(m => n - 1 == m || n + 1 == m));
     }
 
     public class NiceArrayTests
     {
         [Theory]
-        [InlineData(new int[] {2,10,9,3}, true)]
-        [InlineData(new int[] {3,4,5,7}, false)]
+        [InlineData(new[] {2,10,9,3}, true)]
+        [InlineData(new[] {3,4,5,7}, false)]
+        [InlineData(new int[] {}, false)]
         public void VerifyIsNiceWith(int[] arr, bool expectedIsNice)
         {
             NiceArraySolution.IsNice(arr).Should().Be(expectedIsNice);
