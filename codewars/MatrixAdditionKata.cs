634d3d6816bd2c06ad8b0db29ed071be2dbaf66a
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-09-27 12:23:30 +0200

    :green_heart:

--- codewars/MatrixAdditionKata.cs
+++ codewars/MatrixAdditionKata.cs
@@ -9,7 +9,7 @@ namespace codewars
     {
         public static int[][] Add(int[][] a, int[][] b)
         {
-            throw new NotImplementedException();
+            return a.Select((row , i) => row.Zip(b.ElementAt(i), (ea, eb) => ea + eb).ToArray()).ToArray();
         }
     }
 
@@ -18,23 +18,21 @@ namespace codewars
         [Fact]
         public void VerifyAddWith()
         {
-            var a = new[]
+            var a = new int[][]
             {
                 new[] {1, 2},
                 new[] {1, 2}
             };
-            var b = new[]
+            var b = new int[][]
             {
                 new[] {2, 3},
                 new[] {2, 3}
             };
-            var expectedAdd = new[]
-            {
-                new[] {3, 5},
-                new[] {3, 5}
-            };
             
-            MatrixAdditionSolution.Add(a, b).Should().AllBeEquivalentTo(expectedAdd);
+            MatrixAdditionSolution.Add(a, b)[0][0].Should().Be(3);
+            MatrixAdditionSolution.Add(a, b)[0][1].Should().Be(5);
+            MatrixAdditionSolution.Add(a, b)[1][0].Should().Be(3);
+            MatrixAdditionSolution.Add(a, b)[1][1].Should().Be(5);
         }
     }
 }
