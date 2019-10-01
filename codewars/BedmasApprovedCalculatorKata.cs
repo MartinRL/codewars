ead6748d1fe99eb7408533134ca7ed2277f614dc
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-10-01 15:27:38 +0200

    5/5 :green_heart:

--- codewars/BedmasApprovedCalculatorKata.cs
+++ codewars/BedmasApprovedCalculatorKata.cs
@@ -18,6 +18,7 @@ namespace codewars
                 { '+', (a, b ) => a + b },
                 { '/', (a, b ) => a / b },
                 { '-', (a, b ) => a - b },
+                { '^', Pow }
             };
             
             var expression = new string(s.Where(c => !IsWhiteSpace(c)).ToArray());
@@ -53,6 +54,7 @@ namespace codewars
         [InlineData("2*2", 4)]
         [InlineData(" 21,3 /2", 10.65)]
         [InlineData(" 7,3   -  2", 5.3)]
+        [InlineData("2^3", 8)]
         public void VerifyCalculateWith(string s, double expectedCalculated)
         {
             BedmasApprovedCalculatorSolution.Calculate(s).Should().BeApproximately(expectedCalculated, 0.000000001);
