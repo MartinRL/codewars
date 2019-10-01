4451d33113afabdf16ddaa10a33bb38549f659e1
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-10-01 12:25:23 +0200

    1/2 :green_heart:

--- codewars/BedmasApprovedCalculatorKata.cs
+++ codewars/BedmasApprovedCalculatorKata.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using static System.Math;
+using static System.Char;
 using FluentAssertions;
 using Xunit;
 
@@ -9,7 +12,47 @@ namespace codewars
     {
         public static double Calculate(string s)
         {
-            throw new NotImplementedException();
+            var expression = new string(s.Where(c => !IsWhiteSpace(c)).ToArray());
+
+            // 3 = # of operators + number of terms
+            var ss = new List<List<char>> // 2do: Enumerable.Range
+            {
+                new List<char>(),
+                new List<char>(),
+                new List<char>()
+            };
+            
+            var j = 0;
+            for (var i = 0; i < expression.Length; i++)
+            {
+                if (expression[i].IsOperator()) // 2do: hole in the middle
+                {
+                    j++;
+                    ss.ElementAt(j).Add(expression[i]);
+                    j++;
+                }
+                else
+                {
+                    ss.ElementAt(j).Add(expression[i]);    
+                }
+            }
+
+            var sss = ss.Select(_ => _.First().IsOperator() ? _.First().ToString() : new string(_.ToArray()));
+
+            Func<double, double, double> arithmeticFunc = (a, b) => a * b;
+
+            return arithmeticFunc(double.Parse(sss.ElementAt(0)), double.Parse(sss.ElementAt(2)));
+        }
+    }
+
+    public static class BedmasApprovedCalculatorExtensions
+    {
+        public static bool IsOperator(this char @this)
+        {
+            if (@this == '*')
+                return true;
+
+            return false;
         }
     }
 
@@ -20,7 +63,7 @@ namespace codewars
         [InlineData("2*2", 4)]
         public void VerifyCalculateWith(string s, double expectedCalculated)
         {
-            BedmasApprovedCalculatorSolution.Calculate(s).Should().Be(expectedCalculated);
+            BedmasApprovedCalculatorSolution.Calculate(s).Should().BeApproximately(expectedCalculated, 0.000000001);
         }
     }
 }
