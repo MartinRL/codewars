87a69e82544333de4cfe24ab1936101f1993f135
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-11-13 12:13:32 +0100

    all :green:

---
+++ codewars/YouAreASquareKata.cs
@@ -0,0 +1,28 @@
+using static System.Math;
+using FluentAssertions;
+using Xunit;
+
+namespace codewars
+{
+    public class YouAreASquareSolution
+    {
+        public static bool IsSquare(int n)
+        {
+            return Floor(Sqrt(n)) * Floor(Sqrt(n)) == Sqrt(n) * Sqrt(n);
+        }
+    }
+
+    public class YouAreASquareTests
+    {
+        [Theory]
+        [InlineData(-1, false, "negative numbers aren't square numbers")]
+        [InlineData(3, false, "3 isn't a square number")]
+        [InlineData(4, true, "4 is a square number")]
+        [InlineData(25, true, "25 is a square number")]
+        [InlineData(26, false, "26 isN'T a square number")]
+        public void VerifyIsSquareWith(int n, bool expectedIsSquare, string because)
+        {
+            YouAreASquareSolution.IsSquare(n).Should().Be(expectedIsSquare, because);
+        }
+    }
+}
