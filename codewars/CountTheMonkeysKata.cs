19262a75ae2c8cfdbf2fe1d070fb4e4e74b7b6d3
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-11-13 11:54:03 +0100

    :lipstick: expression-bodied x 2

--- codewars/CountTheMonkeysKata.cs
+++ codewars/CountTheMonkeysKata.cs
@@ -7,10 +7,7 @@ namespace codewars
 {
     public class CountTheMonkeysSolution
     {
-        public static int[] MonkeyCount(int n)
-        {
-            return Range(1, n).ToArray();
-        }
+        public static int[] MonkeyCount(int n) => Range(1, n).ToArray();
     }
 
     public class CountTheMonkeysTests
@@ -21,9 +18,6 @@ namespace codewars
         [InlineData(9, new[] {1, 2, 3, 4, 5, 6, 7, 8, 9})]
         [InlineData(10, new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})]
         [InlineData(20, new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})]
-        public void VerifyMonkeyCountWith(int n, int[] monkeys)
-        {
-            CountTheMonkeysSolution.MonkeyCount(n).Should().BeEquivalentTo(monkeys);
-        }
+        public void VerifyMonkeyCountWith(int n, int[] monkeys) => CountTheMonkeysSolution.MonkeyCount(n).Should().BeEquivalentTo(monkeys);
     }
 }
