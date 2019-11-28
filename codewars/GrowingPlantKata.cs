617e80897a171c1bd69227f09b1c5c0fe922d166
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-11-28 12:05:15 +0100

    add :red_circle: test

--- codewars/GrowingPlantKata.cs
+++ codewars/GrowingPlantKata.cs
@@ -2,12 +2,13 @@ using System;
 using System.Linq;
 using FluentAssertions;
 using Xunit;
+using static System.Math;
 
 namespace codewars
 {
     public class GrowingPlantSolution
     {
-        public static int CalculateDaysFor(int upSpeed, int downSpeed, int desiredHeight) => upSpeed >= desiredHeight ? 1 : (desiredHeight / downSpeed - 1) / (upSpeed / downSpeed - 1);
+        public static int CalculateDaysFor(int upSpeed, int downSpeed, int desiredHeight) => upSpeed >= desiredHeight ? 1 : (int)Ceiling((desiredHeight / (decimal)downSpeed - 1) / (upSpeed / (decimal)downSpeed - 1));
     }
 
     public class GrowingPlantTests
@@ -16,6 +17,7 @@ namespace codewars
         [InlineData(100, 10, 910, 10)]
         [InlineData(10, 9, 4, 1)]
         [InlineData(14, 7, 208, 29)]
+        [InlineData(11, 9, 877, 434)]
         public void VerifyCalculateDaysForWith(int upSpeed, int downSpeed, int desiredHeight, int expectedNoOfDays) => GrowingPlantSolution.CalculateDaysFor(upSpeed, downSpeed, desiredHeight).Should().Be(expectedNoOfDays);
     }
 }
