4a21126213b6d9994587aa084a6def2164d13f3a
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-12-02 16:18:34 +0100

    :lipstick:

--- codewars/SupermarketQueueKata.cs
+++ codewars/SupermarketQueueKata.cs
@@ -1,10 +1,10 @@
-using System;
-using System.Linq;
-using FluentAssertions;
-using Xunit;
-
 namespace codewars
 {
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
     public class SupermarketQueueSolution
     {
         public static long CalculateQueueTime(int[] customers, int n)
