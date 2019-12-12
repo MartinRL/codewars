1e675fbf16c46dc4347dd45ade6fb79e93263338
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-12-12 16:49:58 +0100

    boilerplate and tests from codewars
    https://www.codewars.com/kata/hamming-distance-part-1-binary-codes/

---
+++ codewars/HammingDistanceBinaryCodesKata.cs
@@ -0,0 +1,24 @@
+namespace codewars
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
+    public class HammingDistanceBinaryCodesSolution
+    {
+        public static int CalculateDistance(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HammingDistanceBinaryCodesTests
+    {
+        [Theory]
+        [InlineData("100101", "101001", 2)]
+        [InlineData("1010", "0101", 4)]
+        [InlineData("100101011011010010010", "101100010110010110101", 9)]
+        public void VerifyCalculateDistanceWith(string a, string b, int expectedDistance) => HammingDistanceBinaryCodesSolution.CalculateDistance(a, b).Should().Be(expectedDistance);
+    }
+}
