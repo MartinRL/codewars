af9577cb16c07b8e868a764f429da12cd3d799f8
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2019-12-20 09:51:30 +0100

    add tests

--- codewars/DeafRatsOfHamelinKata.cs
+++ codewars/DeafRatsOfHamelinKata.cs
@@ -24,8 +24,11 @@ namespace codewars
     {
         [Theory]
         [InlineData("~0~0~0~0 P", 0)]
+        [InlineData("~0~0~0~0P", 0)]
         [InlineData("P O~ O~ ~O O~", 1)]
+        [InlineData("PO~O~~OO~", 1)]
         [InlineData("~O~O~O~OP~O~OO~", 2)]
+        [InlineData("P~O~OO~", 2)]
         public void VerifyCountDeafRatsWith(string town, int expectedNoOfDeafRats) => DeafRatsOfHamelinSolution.CountDeafRats(town).Should().Be(expectedNoOfDeafRats);
     }
 }
