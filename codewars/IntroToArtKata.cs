a9ffea8f1e3bc3f1fc91a10947d891bbb365e95d
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-01-14 14:05:51 +0100

    2/2 :green_heart:

--- codewars/IntroToArtKata.cs
+++ codewars/IntroToArtKata.cs
@@ -12,13 +12,13 @@ namespace codewars
             if (h < 2)
                 return new string[] {};
 
-            return new[]
-            {
-                "*" + new string(' ', (h - 2) * 2 + 1) + "*" + new string(' ', (h - 2) * 2 + 1) + "*",
-                new string(' ', 1) + "*" + new string(' ', (h - 3) * 2 + 1) + "*" + new string(' ', 1) + "*" + new string(' ', (h - 3) * 2 + 1) + "*" + new string(' ', 1),
-                new string(' ', 2) + "*" + new string(' ', 1) + "*" + new string(' ', (h - 3) * 2 + 1) + "*" + new string(' ', 1) + "*" + new string(' ', 2),
-                new string(' ', 3) + "*" + new string(' ', (h - 2) * 2 + 1) + "*" + new string(' ', 3),
-            };
+            var firstRow = new[] { "*" + new string(' ', (h - 2) * 2 + 1) + "*" + new string(' ', (h - 2) * 2 + 1) + "*" };
+            var lastRow = new[] { new string(' ', h - 1) + "*" + new string(' ', firstRow.First().Length - 2 - (h - 1) * 2) + "*" + new string(' ', h - 1)  };
+
+            return firstRow
+                .Concat(Enumerable.Range(2, h - 2).Select(i => new string(' ', i - 1) + "*" + new string(' ', (h - 3) * 2 + 1) + "*" + new string(' ', 1) + "*" + new string(' ', (h - 3) * 2 + 1) + "*" + new string(' ', i - 1)))
+                .Concat(lastRow)
+                .ToArray();
         }
     }
 
