c18dd93aa160c1f27b9b38d932a457b07c43a21b
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-01-24 11:41:05 +0100

    1/5 :green_heart:

--- codewars/RangeExtractionKata.cs
+++ codewars/RangeExtractionKata.cs
@@ -1,18 +1,33 @@
 namespace codewars
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using FluentAssertions;
     using Xunit;
 
     public class RangeExtractionSolution
     {
-        public static string Extract(int[] args) => string.Join(",", args);
+        public static string Extract(int[] args)
+        {
+            if (!args.Any())
+                return string.Empty;
+
+            var argsQueue = new Queue<int>(args);
+
+            /*while (argsQueue.Any())
+            {
+
+            }*/
+
+            return "";
+        }
     }
 
     public class RangeExtractionTests
     {
         [Theory]
+        [InlineData(new int[0], "")]
         [InlineData(new[] { 1, 2 }, "1,2")]
         [InlineData(new[] { 1, 2, 3 }, "1-3")]
         [InlineData(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 }, "-6,-3-1,3-5,7-11,14,15,17-20")]
