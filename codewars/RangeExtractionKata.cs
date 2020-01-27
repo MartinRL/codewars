acaf746c7f000e907aa31a9be57b727a646fbd9c
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-01-27 13:43:21 +0100

    add :red_circle: test

--- codewars/RangeExtractionKata.cs
+++ codewars/RangeExtractionKata.cs
@@ -42,6 +42,7 @@ namespace codewars
         [InlineData(new int[0], "")]
         [InlineData(new[] { 1 }, "1")]
         [InlineData(new[] { 1, 2 }, "1,2")]
+        [InlineData(new[] { 1, 5 }, "1,5")]
         [InlineData(new[] { 1, 2, 3 }, "1-3")]
         [InlineData(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 }, "-6,-3-1,3-5,7-11,14,15,17-20")]
         [InlineData(new[] { -3, -2, -1, 2, 10, 15, 16, 18, 19, 20 }, "-3--1,2,10,15,16,18-20")]
