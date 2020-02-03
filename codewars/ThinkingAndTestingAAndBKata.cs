4a7d7d8c814fc2d830574de6eacfb039a04a9e18
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-03 13:00:25 +0100

    3/5 :green_heart:

--- codewars/ThinkingAndTestingAAndBKata.cs
+++ codewars/ThinkingAndTestingAAndBKata.cs
@@ -7,17 +7,17 @@ namespace codewars
 
     public class ThinkingAndTestingAAndBSolution
     {
-        public static int TestIt(int a, int b) => throw new NotImplementedException();
+        public static int TestIt(int a, int b) => a + (b - a);
     }
 
     public class ThinkingAndTestingAAndBTests
     {
         [Theory]
-        [InlineData(0, 1, 1)]
+        [InlineData(0, 1, 1)] // 0 + (1 - 0) = 1
         [InlineData(1, 2, 3)]
         [InlineData(10, 20, 30)]
         [InlineData(1, 1, 1)]
-        [InlineData(1, 3, 3)]
+        [InlineData(1, 3, 3)] // 1 + (3 - 1) = 3
         public void VerifyTestItWith(int a, int b, int expected) => ThinkingAndTestingAAndBSolution.TestIt(a, b).Should().Be(expected);
     }
 }
