28fb3594261a815a95e88258c65e5b92575c8381
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-03 12:39:42 +0100

    boilerplate and test cases from codewars
    https://www.codewars.com/kata/56d904db9963e9cf5000037d

---
+++ codewars/ThinkingAndTestingAAndBKata.cs
@@ -0,0 +1,23 @@
+namespace codewars
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ThinkingAndTestingAAndBSolution
+    {
+        public static int TestIt(int a, int b) => throw new NotImplementedException();
+    }
+
+    public class ThinkingAndTestingAAndBTests
+    {
+        [Theory]
+        [InlineData(0, 1, 1)]
+        [InlineData(1, 2, 3)]
+        [InlineData(10, 20, 30)]
+        [InlineData(1, 1, 1)]
+        [InlineData(1, 3, 3)]
+        public void VerifyTestItWith(int a, int b, int expected) => ThinkingAndTestingAAndBSolution.TestIt(a, b).Should().Be(expected);
+    }
+}
