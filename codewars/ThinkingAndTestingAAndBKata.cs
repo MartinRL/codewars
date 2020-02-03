1d8c6108ea81819841fd1a7ae3c29071b04454ed
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-03 13:16:44 +0100

    5/5 :green_heart:

--- codewars/ThinkingAndTestingAAndBKata.cs
+++ codewars/ThinkingAndTestingAAndBKata.cs
@@ -7,17 +7,27 @@ namespace codewars
 
     public class ThinkingAndTestingAAndBSolution
     {
-        public static int TestIt(int a, int b) => a + (b - a);
+        public static int TestIt(int a, int b) => a | b;
     }
 
     public class ThinkingAndTestingAAndBTests
     {
         [Theory]
-        [InlineData(0, 1, 1)] // 0 + (1 - 0) = 1
+        // 0
+        // 1
+        [InlineData(0, 1, 1)]
+        // 01
+        // 10
         [InlineData(1, 2, 3)]
+        // 01010
+        // 10100
         [InlineData(10, 20, 30)]
+        // 1
+        // 1
         [InlineData(1, 1, 1)]
-        [InlineData(1, 3, 3)] // 1 + (3 - 1) = 3
+        // 01
+        // 11
+        [InlineData(1, 3, 3)]
         public void VerifyTestItWith(int a, int b, int expected) => ThinkingAndTestingAAndBSolution.TestIt(a, b).Should().Be(expected);
     }
 }
