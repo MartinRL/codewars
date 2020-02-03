87bc707c81a3f811136d151002c959738505ece7
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-03 13:17:46 +0100

    :lipstick:

--- codewars/ThinkingAndTestingAAndBKata.cs
+++ codewars/ThinkingAndTestingAAndBKata.cs
@@ -15,17 +15,22 @@ namespace codewars
         [Theory]
         // 0
         // 1
+        // 1
         [InlineData(0, 1, 1)]
         // 01
         // 10
+        // 11
         [InlineData(1, 2, 3)]
         // 01010
         // 10100
+        // 11110
         [InlineData(10, 20, 30)]
+        // 1
         // 1
         // 1
         [InlineData(1, 1, 1)]
         // 01
+        // 11
         // 11
         [InlineData(1, 3, 3)]
         public void VerifyTestItWith(int a, int b, int expected) => ThinkingAndTestingAAndBSolution.TestIt(a, b).Should().Be(expected);
