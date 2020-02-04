d0df057177bb243bcf66cd90a6f9d5bfeb89d144
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-04 08:38:13 +0100

    8/8 :green_heart:

--- codewars/ThinkingAndTestingIncompleteStringKata.cs
+++ codewars/ThinkingAndTestingIncompleteStringKata.cs
@@ -7,7 +7,22 @@ namespace codewars
 
     public class ThinkingAndTestingIncompleteStringSolution
     {
-        public static string TestIt(string s) => throw new NotImplementedException();
+        public static string TestIt(string s)
+        {
+            var modString = string.Empty;
+
+            for(var i = 0; i < s.Length; i += 2)
+            {
+                var meanChar = s[i];
+                if(i + 1 < s.Length)
+                {
+                    meanChar = (char)((meanChar + s[i+1]) / 2);
+                }
+                modString += meanChar;
+            }
+
+            return modString;
+        }
     }
 
     public class ThinkingAndTestingIncompleteStringTests
@@ -19,8 +34,8 @@ namespace codewars
         [InlineData("aa", "a")]
         [InlineData("ab", "a")]
         [InlineData("bc", "b")]
-        [InlineData("aa", "aaaa")]
-        [InlineData("aaa", "aaaaaa")]
+        [InlineData("aaaa", "aa")]
+        [InlineData("aaaaaa", "aaa")]
         public void VerifyTestItWith(string s, string expected) => ThinkingAndTestingIncompleteStringSolution.TestIt(s).Should().Be(expected);
     }
 }
