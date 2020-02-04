ca202435223d879ce9253970720478c0c5b4410e
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-04 15:21:46 +0100

    boilerplate and test cases from codewars
    https://www.codewars.com/kata/56d93f249c844788bc000002/

---
+++ codewars/ThinkingAndTestingSomethingCapitalizedKata.cs
@@ -0,0 +1,21 @@
+namespace codewars
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ThinkingAndTestingSomethingCapitalizedSolution
+    {
+        public static string TestIt(string s) => throw new NotImplementedException();
+    }
+
+    public class ThinkingAndTestingSomethingCapitalizedTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "A")]
+        [InlineData("a b c", "A B C")]
+        public void VerifyTestItWith(string s, string expected) => ThinkingAndTestingSomethingCapitalizedSolution.TestIt(s).Should().Be(expected);
+    }
+}
