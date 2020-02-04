a98fe4196581a519d90ced17bd71e939a6240606
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-04 15:27:29 +0100

    change to one :red_circle: test

--- codewars/ThinkingAndTestingSomethingCapitalizedKata.cs
+++ codewars/ThinkingAndTestingSomethingCapitalizedKata.cs
@@ -16,7 +16,7 @@ namespace codewars
         [InlineData("", "")]
         [InlineData("a", "A")]
         [InlineData("a b c", "A B C")]
-        [InlineData("Aa", "aA")]
+        [InlineData("aa", "aA")]
         public void VerifyTestItWith(string s, string expected) => ThinkingAndTestingSomethingCapitalizedSolution.TestIt(s).Should().Be(expected);
     }
 }
