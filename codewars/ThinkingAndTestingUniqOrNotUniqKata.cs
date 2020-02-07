4a34a4a30d1c0cf14b3a3628d0bfa7ef22d3e456
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-07 14:59:28 +0100

    5/5 correct solution

--- codewars/ThinkingAndTestingUniqOrNotUniqKata.cs
+++ codewars/ThinkingAndTestingUniqOrNotUniqKata.cs
@@ -7,7 +7,7 @@ namespace codewars
 
     public class ThinkingAndTestingUniqOrNotUniqSolution
     {
-        public static int[] TestIt(int[] a, int[] b) => a.Concat(b).OrderBy(_ => _).ToArray();
+        public static int[] TestIt(int[] a, int[] b) => a.Distinct().Concat(b.Distinct()).OrderBy(x => x).ToArray();
     }
 
     public class ThinkingAndTestingUniqOrNotUniqTests
