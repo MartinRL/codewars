726f1545d4b0a20abea60ae7b016872396233f02
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-02-12 09:25:22 +0100

    add :red_circle: test

--- codewars/ThinkingAndTestingMathOfMiddleSchoolKata.cs
+++ codewars/ThinkingAndTestingMathOfMiddleSchoolKata.cs
@@ -1,6 +1,5 @@
 namespace codewars
 {
-    using System;
     using System.Linq;
     using FluentAssertions;
     using Xunit;
@@ -28,6 +27,7 @@ namespace codewars
         [InlineData(new[] {22, 88, 0, 0}, new[] {0, 100, 0, 100}, new[] {0, 11000, 0, 0})]
         [InlineData(new[] {0, 0, 96, 49}, new[] {86, 0, 53, 0}, new[] {0, 0, 10853, 0})]
         [InlineData(new[] {0, 0, 83, 88}, new[] {0, 27, 0, 99}, new[] {0, 0, 0, 10953})]
+        [InlineData(new[] {82, 31, 65, 39}, new[] {55, 36, 21, 48}, new[] {5161, 4440, 4394, 4212})]
         public void VerifyTestItWith(int[] a, int[] b, int[] expected) => ThinkingAndTestingMathOfMiddleSchoolSolution.TestIt(a, b).Should().BeEquivalentTo(expected);
     }
 }
