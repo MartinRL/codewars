8952fd162addf4ff046e465da5f6964856a902c8
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-05-07 15:26:09 +0200

    4/4 :green_heart:
    sln stolen from codewars

--- codewars/Properties/RectangleRotationKata.cs
+++ codewars/Properties/RectangleRotationKata.cs
@@ -7,7 +7,32 @@ namespace codewars
 
     public class RectangleRotationSolution
     {
-        public static int RectangleRotation(int a, int b) => throw new NotImplementedException();
+        public static int RectangleRotation(int a, int b)
+        {
+            var sqrt2 = Math.Sqrt(2);
+            double ah = a / 2f;
+            double bh = b / 2f;
+            double Top(double x) => ah * sqrt2 - x;
+            double Bottom(double x) => -ah * sqrt2 - x;
+            double Right(double x) => -bh * sqrt2 + x;
+            double Left(double x) => bh * sqrt2 + x;
+            int CalcColumn(int x)
+            {
+                var max = Math.Floor(Math.Min(Top(x), Left(x)));
+                var min = Math.Ceiling(Math.Max(Right(x), Bottom(x)));
+                return (int)Math.Round(max - min) + 1;
+            }
+            var highest = sqrt2 * (ah + bh) / 2;
+
+            var rightSide = 0;
+            for (int column = 1; column < highest; column++)
+            {
+                rightSide += CalcColumn(column);
+            }
+
+            var column0 = CalcColumn(0);
+            return column0 + 2 * rightSide;
+        }
     }
 
     public class RectangleRotationTests
