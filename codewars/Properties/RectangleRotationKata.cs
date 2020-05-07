080ba4c3aa069e2243f5865bd465512da2bef5e7
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-05-07 15:40:05 +0200

    :lipstick: using static

--- codewars/Properties/RectangleRotationKata.cs
+++ codewars/Properties/RectangleRotationKata.cs
@@ -1,15 +1,14 @@
 namespace codewars
 {
-    using System;
-    using System.Linq;
     using FluentAssertions;
     using Xunit;
+    using static System.Math;
 
     public class RectangleRotationSolution
     {
         public static int RectangleRotation(int a, int b)
         {
-            var sqrt2 = Math.Sqrt(2);
+            var sqrt2 = Sqrt(2);
             double ah = a / 2f;
             double bh = b / 2f;
             double Top(double x) => ah * sqrt2 - x;
@@ -18,9 +17,9 @@ namespace codewars
             double Left(double x) => bh * sqrt2 + x;
             int CalcColumn(int x)
             {
-                var max = Math.Floor(Math.Min(Top(x), Left(x)));
-                var min = Math.Ceiling(Math.Max(Right(x), Bottom(x)));
-                return (int)Math.Round(max - min) + 1;
+                var max = Floor(Min(Top(x), Left(x)));
+                var min = Ceiling(Max(Right(x), Bottom(x)));
+                return (int)Round(max - min) + 1;
             }
             var highest = sqrt2 * (ah + bh) / 2;
 
