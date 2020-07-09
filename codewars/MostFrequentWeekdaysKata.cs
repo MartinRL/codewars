7b18bb5cb4831297a916d1b7af96c896aee77d77
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-07-09 09:04:12 +0200

    boilerplate and tests from codewars
    https://www.codewars.com/kata/56eb16655250549e4b0013f4/

---
+++ codewars/MostFrequentWeekdaysKata.cs
@@ -0,0 +1,36 @@
+namespace codewars
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
+    public class MostFrequentWeekdaysSolution
+    {
+        public static string[] GetMostFrequentDaysOf(int year) => throw new NotImplementedException();
+    }
+
+    public class MostFrequentWeekdaysTests
+    {
+        [Theory]
+        [InlineData(2427, new[] {"Friday"})]
+        [InlineData(2185, new[] {"Saturday"})]
+        [InlineData(2860, new[] {"Thursday", "Friday"})]
+        [InlineData(2016, new[] {"Friday", "Saturday"})]
+        [InlineData(1770, new[] {"Monday"})]
+        [InlineData(2001, new[] {"Monday"})]
+        [InlineData(1968, new[] {"Monday", "Tuesday"})]
+        [InlineData(1785, new[] {"Saturday"})]
+        [InlineData(1910, new[] {"Saturday"})]
+        [InlineData(2135, new[] {"Saturday"})]
+        [InlineData(3043, new[] {"Sunday"})]
+        [InlineData(3150, new[] {"Sunday"})]
+        [InlineData(3361, new[] {"Thursday"})]
+        [InlineData(1901, new[] {"Tuesday"})]
+        [InlineData(3230, new[] {"Tuesday"})]
+        [InlineData(1794, new[] {"Wednesday"})]
+        [InlineData(1986, new[] {"Wednesday"})]
+        public void VerifyMostFrequentDaysWith(int year, string[] expectedWeekdays) =>
+            MostFrequentWeekdaysSolution.GetMostFrequentDaysOf(year).Should().Equal(expectedWeekdays);
+    }
+}
