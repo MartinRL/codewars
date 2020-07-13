89102509e090e5cdd186206a20208d23f97e0c52
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-07-13 13:44:38 +0200

    3/3 :green_heart:

--- codewars/ChessBoardCellColorKata.cs
+++ codewars/ChessBoardCellColorKata.cs
@@ -1,8 +1,6 @@
 namespace codewars
 {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
     using System.Drawing;
     using System.Linq;
     using FluentAssertions;
@@ -10,32 +8,20 @@ namespace codewars
 
     public class ChessBoardCellColorSolution
     {
-        public static Color GetColorOfCell(string cell) => ((int)cell.First() % 2 == 0 && (int)char.GetNumericValue(cell.Last()) % 2 == 0) ? Color.White : Color.Black;
+        private static Color GetColorOfCell(string cell)=> ((int)cell.First() % 2, (int)cell.Last() % 2) switch
+            {
+                (1, 1) => Color.Black,
+                (0, 0) => Color.Black,
+                _ => Color.White
+            };
 
-        public static bool HaveSameColors(string cell1, string cell2) => throw new NotImplementedException();
+        public static bool HaveSameColors(string cell1, string cell2) => GetColorOfCell(cell1) == GetColorOfCell(cell2);
     }
 
     public class ChessBoardCellColorTests
     {
-        private class GetColorOfCellTestData : IEnumerable<object[]>
-        {
-            public IEnumerator<object[]> GetEnumerator()
-            {
-                yield return new object[] { "A1", Color.Black };
-                yield return new object[] { "C3", Color.Black };
-                yield return new object[] { "H3", Color.White };
-                yield return new object[] { "A2", Color.White };
-            }
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }
 
         [Theory]
-        [ClassData(typeof(GetColorOfCellTestData))]
-        public void GetColorOfCell(string cell, Color expected) => ChessBoardCellColorSolution.GetColorOfCell(cell).Should().Be(expected);
-
-        [Theory]
-
         [InlineData("A1", "C3", true)]
         [InlineData("A1", "H3", false)]
         [InlineData("A1", "A2", false)]
