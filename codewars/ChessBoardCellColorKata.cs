9d180984e063bb9f9378ee831f9cf0a7c372c02f
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-07-13 14:10:50 +0200

    add and fix :red: test --> 5/5 :green_heart:

--- codewars/ChessBoardCellColorKata.cs
+++ codewars/ChessBoardCellColorKata.cs
@@ -7,7 +7,9 @@ namespace codewars
 
     public class ChessBoardCellColorSolution
     {
-        public static bool HaveSameColors(string cell1, string cell2) => (cell1.First() % 2 == cell1.Last() % 2) && (cell2.First() % 2 == cell2.Last() % 2);
+        public static bool HaveSameColors(string cell1, string cell2) =>
+            (cell1.First() % 2 == cell1.Last() % 2) && (cell2.First() % 2 == cell2.Last() % 2) ||
+            (cell1.First() % 2 != cell1.Last() % 2) && (cell2.First() % 2 != cell2.Last() % 2);
     }
 
     public class ChessBoardCellColorTests
@@ -17,6 +19,8 @@ namespace codewars
         [InlineData("A1", "C3", true)]
         [InlineData("A1", "H3", false)]
         [InlineData("A1", "A2", false)]
+        [InlineData("E7", "E3", true)]
+        [InlineData("D1", "B5", true)]
         public void VerifyHaveSameColorsWith(string cell1, string cell2, bool expected) => ChessBoardCellColorSolution.HaveSameColors(cell1, cell2).Should().Be(expected);
     }
 }
