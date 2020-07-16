a589665dc3a7d0b716c21ab9c70fdb8b052e904c
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-07-16 09:10:43 +0200

    6/6 :green_heart:
    codewars obviously doesn't allow me to pull in external nuget pkgs, but this is how I'd solve this IRL

--- codewars/CalculatorKata.cs
+++ codewars/CalculatorKata.cs
@@ -1,19 +1,24 @@
+using codewars;
+using FluentAssertions;
+using Xunit;
+
 namespace codewars
 {
-    using System;
-    using System.Linq;
+    using static System.Double;
+    using static Microsoft.CodeAnalysis.CSharp.Scripting.CSharpScript;
     using FluentAssertions;
     using Xunit;
 
     public class CalculatorSolution
     {
-        public static double Evaluate(string expression) => throw new NotImplementedException();
+        public static double Evaluate(string expression) => Parse(EvaluateAsync(expression).Result.ToString());
     }
 
     public class CalculatorTests
     {
         [Theory]
         [InlineData("2 / 2 + 3 * 4 - 6", 7)]
+        [InlineData("2 / 2 + 3 * 4 / 2 - 6", 1)]
         [InlineData("1+1", 2)]
         [InlineData("1 - 1", 0)]
         [InlineData("1* 1", 1)]
