f734c25cf0e87718559881d77b2a7dfc9053cf57
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-07-22 12:41:44 +0200

    3/3 :green_heart:

---
+++ codewars/FakeBinaryKata.cs
@@ -0,0 +1,23 @@
+namespace codewars
+{
+    using System;
+    using System.Linq;
+    using static System.Convert;
+    using static System.String;
+    using FluentAssertions;
+    using Xunit;
+
+    public class FakeBinarySolution
+    {
+        public static string Fake(string s) => Concat(s.Select(c => ToByte(c) < 53 ? '0' : '1'));
+    }
+
+    public class FakeBinaryTests
+    {
+        [Theory]
+        [InlineData("45385593107843568", "01011110001100111")]
+        [InlineData("509321967506747", "101000111101101")]
+        [InlineData("366058562030849490134388085", "011011110000101010000011011")]
+        public void VerifyFakeWith(string s, string expectedFake) => FakeBinarySolution.Fake(s).Should().Be(expectedFake);
+    }
+}
