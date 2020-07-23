0aa54b6471e71660aae542a696672eebeb90ce80
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2020-07-23 13:19:12 +0200

    13/16 :green_heart:

--- codewars/RankingPokerHandsKata.cs
+++ codewars/RankingPokerHandsKata.cs
@@ -125,12 +125,22 @@ namespace codewars
 
             if (HandValue == hand.HandValue)
             {
-                if (HandValue == HandValue.Pair || HandValue == HandValue.TwoPairs || HandValue == HandValue.ThreeOfAKind || HandValue == HandValue.FourOfAKind ||
+                if (HandValue == HandValue.ThreeOfAKind || HandValue == HandValue.FourOfAKind ||
                     HandValue == HandValue.FullHouse)
                     return this.hand.GroupBy(c => c.Value).OrderByDescending(g => g.Count()).First().Key >
                            hand.hand.GroupBy(c => c.Value).OrderByDescending(g => g.Count()).First().Key
-                        ? Result.Win
-                        : Result.Loss;
+                        ? Result.Win : Result.Loss;
+
+                if (HandValue == HandValue.StraightFlush || HandValue == HandValue.Flush)
+                {
+                    if (this.hand.ElementAt(4).Value > hand.hand.ElementAt(4).Value)
+                        return Result.Win;
+
+                    if (this.hand.ElementAt(4).Value < hand.hand.ElementAt(4).Value)
+                        return Result.Loss;
+
+                    return Result.Tie;
+                }
             }
 
             /*if (this.hand.Last().CompareTo(hand.hand.Last()) == 1)
