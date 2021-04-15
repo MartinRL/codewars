eb0c8a88534f3dfe48288f53f129292ae0d26dd1
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2021-04-15 13:03:10 +0200

    WeightForWeightKataExtensions 2/2 :white_check_mark:

--- codewars/WeightForWeightKata.cs
+++ codewars/WeightForWeightKata.cs
@@ -1,13 +1,19 @@
 namespace codewars
 {
     using System;
+    using System.Globalization;
     using System.Linq;
     using FluentAssertions;
     using Xunit;
 
     public class WeightForWeightKataSolution
     {
-        public static string OrderWeight(string weights) => throw new NotImplementedException();
+        public static string OrderWeight(string weights) => weights.Split(" ").OrderBy(_ => _.WeightedWeight()).Aggregate((_, __) => $"{_} {__}");
+    }
+
+    public static class WeightForWeightKataExtensions
+    {
+        public static long WeightedWeight(this string @this) => @this.Sum(c => uint.Parse(c.ToString(), NumberStyles.Integer));
     }
 
     public class WeightForWeightKataTests
@@ -16,5 +22,13 @@ namespace codewars
         [InlineData("2000 103 123 4444 99", "103 123 4444 99 2000")]
         [InlineData("11 11 2000 10003 22 123 1234000 44444444 9999", "2000 10003 1234000 44444444 9999 11 11 22 123")]
         public void VerifyOrderWeightWith(string weights, string expectedOrderedWeights) => WeightForWeightKataSolution.OrderWeight(weights).Should().Be(expectedOrderedWeights);
+    }
+
+    public class WeightForWeightExtensionsTests
+    {
+        [Theory]
+        [InlineData("99", 18)]
+        [InlineData("100", 1)]
+        public void VerifyOrderWeightWith(string weight, long expectedWeightedWeight) => weight.WeightedWeight().Should().Be(expectedWeightedWeight);
     }
 }
