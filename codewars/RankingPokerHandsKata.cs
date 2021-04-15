6e648651338f8261e91503d2c0eec3e56cfd08fb
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2021-04-15 10:39:10 +0200

    it was actually a test case @ codewars, so I don't grasp the spec...

--- codewars/RankingPokerHandsKata.cs
+++ codewars/RankingPokerHandsKata.cs
@@ -1,9 +1,7 @@
 namespace codewars
 {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
-    using System.Collections.Immutable;
     using System.Linq;
     using FluentAssertions;
     using Xunit;
@@ -195,7 +193,7 @@ namespace codewars
         [InlineData("Straight wins of three of a kind", Result.Win, "2S 3H 4H 5S 6C", "AH AC 5H 6H AS")]
         [InlineData("3 Of a kind wins of two pair", Result.Loss, "2S 2H 4H 5S 4C", "AH AC 5H 6H AS")]
         [InlineData("2 Pair wins of pair", Result.Win, "2S 2H 4H 5S 4C", "AH AC 5H 6H 7S")]
-        [InlineData("Same pair, settled by kicker (5 > 4)", Result.Loss, "6S AD 7H 4S AS", "AH AC 5H 6H 7S")]
+        [InlineData("Highest pair wins", Result.Loss, "6S AD 7H 4S AS", "AH AC 5H 6H 7S")]
         [InlineData("Pair wins of nothing", Result.Loss, "2S AH 4H 5S KC", "AH AC 5H 6H 7S")]
         [InlineData("Highest card loses", Result.Loss, "2S 3H 6H 7S 9C", "7H 3C TH 6H 9S")]
         [InlineData("Highest card wins", Result.Win, "4S 5H 6H TS AC", "3S 5H 6H TS AC")]
