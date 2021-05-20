ba98061119ea52e7b168449bb43b50dbc86fc52f
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2021-05-20 15:26:16 +0200

    optimisation 5/9 :white_check_mark:

--- codewars/PlayingOnAChessboardKata.cs
+++ codewars/PlayingOnAChessboardKata.cs
@@ -12,20 +12,7 @@ namespace codewars
             if (n == 0)
                 return "[0]";
 
-            var fraction = new Fraction();
-
-            for (var i = 1; i <= n; i++)
-            {
-                for (var j = 1; j <= n; j++)
-                {
-                    fraction += new Fraction(i, j + i);
-                }
-            }
-
-            if (fraction.Denominator == 1)
-                return $"[{fraction.Numerator}]";
-
-            return $"[{fraction.Numerator}, {fraction.Denominator}]";
+            return $"[{n * n}, 2]";
         }
     }
 
@@ -391,6 +378,11 @@ namespace codewars
         [InlineData(0, "[0]")]
         [InlineData(1, "[1, 2]")]
         [InlineData(2, "[2]")]
+        [InlineData(3, "[9, 2]")]
+        [InlineData(4, "[8]")]
+        [InlineData(5, "[25, 2]")]
+        [InlineData(6, "[18]")]
+        [InlineData(7, "[49, 2]")]
         [InlineData(8, "[32]")]
         //[InlineData(10000, "dummy")]
         public void VerifyGameWith(BigInteger n, string expectedGameOutcome) => PlayingOnAChessboardSolution.Game(n).Should().Be(expectedGameOutcome);
