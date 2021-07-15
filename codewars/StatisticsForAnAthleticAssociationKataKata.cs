79b9d907cdf848c02849be4e98efa0c6223eed4c
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2021-07-15 15:16:46 +0200

    boilerplate and tests from codewars
    https://www.codewars.com/kata/55b3425df71c1201a800009c

---
+++ codewars/StatisticsForAnAthleticAssociationKataKata.cs
@@ -0,0 +1,20 @@
+namespace codewars
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
+    public class StatisticsForAnAthleticAssociationSolution
+    {
+        public static string CalculateStats(string teamResults) => throw new NotImplementedException();
+    }
+
+    public class StatisticsForAnAthleticAssociationTests
+    {
+        [Theory]
+        [InlineData("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17", "Range: 01|01|18 Average: 01|38|05 Median: 01|32|34")]
+        [InlineData("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41", "Range: 00|31|17 Average: 02|26|18 Median: 02|22|00")]
+        public void VerifyCalculateStatsWith(string teamResults, string expectedStats) => StatisticsForAnAthleticAssociationSolution.CalculateStats(teamResults).Should().Be(expectedStats);
+    }
+}
