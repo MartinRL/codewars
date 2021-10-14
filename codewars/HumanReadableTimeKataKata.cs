6407d3fcb8306b235b2d9b7f36886c86a986918a
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2021-10-14 13:17:54 +0200

    5/6 :white_check_mark: add failing test

--- codewars/HumanReadableTimeKataKata.cs
+++ codewars/HumanReadableTimeKataKata.cs
@@ -32,6 +32,7 @@ namespace codewars
         [InlineData(60, "00:01:00")]
         [InlineData(86399, "23:59:59")]
         [InlineData(359999, "99:59:59")]
+        [InlineData(24 * 60 * 60, "24:00:00")]
         public void VerifyGetReadableTimeWith(int seconds, string expectedReadableTime) => HumanReadableTimeSolution.GetReadableTime(seconds).Should().Be(expectedReadableTime);
     }
 }
