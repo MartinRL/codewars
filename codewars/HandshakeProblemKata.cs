d5604e30112405ec79cd8d0c4bb8f3e339bb2e73
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2022-01-06 08:36:36 +0100

    boilerplate and tests from codewars
    https://www.codewars.com/kata/5574835e3e404a0bed00001b/train/csharp

---
+++ codewars/HandshakeProblemKata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars;
+
+public static class HandshakeProblemSolution
+{
+    public static int GetParticipants(int handshakes)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class HandshakeProblemTests
+{
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    [InlineData(3, 3)]
+    [InlineData(6, 4)]
+    [InlineData(7, 5)]
+    public void VerifyGetParticipantsWith(int handshakes, int expectedParticipants) 
+        => HandshakeProblemSolution.GetParticipants(handshakes).Should().Be(expectedParticipants);
+}
