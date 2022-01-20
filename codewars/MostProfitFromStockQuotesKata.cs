f043dcd2833178c2afb833945a9273df32d9e0fd
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2022-01-20 14:56:19 +0100

    boilerplate and tests from codewars
    https://www.codewars.com/kata/597ef546ee48603f7a000057/train/csharp

---
+++ codewars/MostProfitFromStockQuotesKata.cs
@@ -0,0 +1,15 @@
+﻿namespace codewars;
+
+internal static class MostProfitFromStockQuoteSolution
+{
+    public static int GetMostProfitFromStockQuotes(int[] quotes) => throw new NotImplementedException();
+}
+
+public class MostProfitFromStockQuoteTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, 15)]
+    [InlineData(new[] { 6, 5, 4, 3, 2, 1 }, 0)]
+    [InlineData(new[] { 1, 6, 5, 10, 8, 7 }, 18)]
+    public void VerifyGetMostProfitFromStockQuotesWith(int[] quotes, int expectedMostProfit) => MostProfitFromStockQuoteSolution.GetMostProfitFromStockQuotes(quotes).Should().Be(expectedMostProfit);
+}
