8028a298f65f92c4a22fb327f996711e04a848e6
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2023-06-09 11:50:17 +0200

    codewars tests and boilerplate

--- codewars/RegularExpressionCheckIfDivisibleBy0b111Kata.cs
+++ codewars/RegularExpressionCheckIfDivisibleBy0b111Kata.cs
@@ -1,16 +1,32 @@
-﻿namespace codewars;
+﻿using Xunit.Abstractions;
+
+namespace codewars;
 
 public class RegularExpressionCheckIfDivisibleBy0b111Solution
 {
-    public static string MultipleOf7() {
-        // Write a *string* which represents a regular expression to detect whether a binary number is divisible by 7
-        return "";
-    }
+    public static string MultipleOf7() => @"(?<regex>
+    # The base case: single digits that are multiples of 7
+    (?:0|111) |
+    # The recursive case: any number that satisfies the rule
+    (?:
+        # Capture the last three bits and the rest of the number
+        (000|001|010|011|100|101|110|111) (.*)
+        # Check if the last three bits plus four times the rest of the number matches the regex
+        (?= (?&regex))
+    )
+)";
 }
 
 public class RegularExpressionCheckIfDivisibleBy0b111Tests
 {
     private static string rgxStr = RegularExpressionCheckIfDivisibleBy0b111Solution.MultipleOf7();
+    
+    private readonly ITestOutputHelper output;
+
+    public RegularExpressionCheckIfDivisibleBy0b111Tests(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
     
     [Fact]
     public void EdgeCases()
@@ -25,6 +41,8 @@ public class RegularExpressionCheckIfDivisibleBy0b111Tests
     {
         for (var i=1; i<100; i++) 
         {
+            output.WriteLine(i.ToString());
+
             Regex.Match(Convert.ToString(i, 2), rgxStr).Success.Should().Be(i%7 == 0);
         }
     }
