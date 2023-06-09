4bf71fdf4a7bf48233b7cf6d63688acb12efb739
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2023-06-09 12:24:29 +0200

    codewars boilerplate and tests
    https://www.codewars.com/kata/56bdd0aec5dc03d7780010a5/train/csharp

---
+++ codewars/FindNextHigherNumberWithSameBitsKata.cs
@@ -0,0 +1,21 @@
+﻿namespace codewars;
+
+public static class FindNextHigherNumberWithSameBitsSolution
+{
+    public static int Find(int number)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class FindNextHigherNumberWithSameBitsTests
+{
+    [Theory]
+    [InlineData(128, 256)]
+    [InlineData(1, 2)]
+    [InlineData(1022, 1279)]
+    [InlineData(127, 191)]
+    [InlineData(1253343, 1253359)]
+    public void VerifyNextHigherNumberWithSameBitsWith(int number, int expectdNextHigherNumberWithSameBits) 
+        => FindNextHigherNumberWithSameBitsSolution.Find(number).Should().Be(expectdNextHigherNumberWithSameBits);
+}
