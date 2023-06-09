c4b343d30daa07e685c891e36b56707818794d24
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2023-06-09 15:14:20 +0200

    :lipstick: comments since I'm bad at understanding this kind of arithmetic these days

--- codewars/FindNextHigherNumberWithSameBitsKata.cs
+++ codewars/FindNextHigherNumberWithSameBitsKata.cs
@@ -5,14 +5,26 @@ public static class FindNextHigherNumberWithSameBitsSolution
     public static int Find(int num)
     {
         int o, n, p, r;
-        
-        o = num & -num;
-        n = num + o;
-        p = num ^ n;
-        p /= o;
-        p >>= 2;
-        r = n | p;
     
+        // Find the rightmost set bit of num
+        o = num & -num;
+
+        // Flip the rightmost set bit and all bits to the right of it
+        n = num + o;
+
+        // Compute the XOR of num and n
+        p = num ^ n;
+
+        // Divide p by o, effectively shifting the bits to the right
+        p /= o;
+
+        // Right-shift p by 2 positions, equivalent to dividing by 4
+        p >>= 2;
+
+        // Combine the rightmost one chunk and the number of ones to move
+        r = n | p;
+
+        // Return the next higher number with the same number of set bits
         return r;
     }
 }
