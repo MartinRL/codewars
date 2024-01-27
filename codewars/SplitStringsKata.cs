f71d606dbdb0b5f9bdf33be81287956c15d875f0
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-01-27 13:45:27 +0100

    boilerplate and codewars tests
    https://www.codewars.com/kata/515de9ae9dcfc28eb6000001

---
+++ codewars/SplitStringsKata.cs
@@ -0,0 +1,14 @@
+﻿namespace codewars;
+
+public class SplitStringSolution
+{
+    public static string[] SplitString(string str) => throw new NotImplementedException();
+}
+
+public class SplitStringTests
+{
+    [Theory]
+    [InlineData("abc", new string[] { "ab", "c_" })]
+    [InlineData("abcdef", new string[] { "ab", "cd", "ef" })]
+    public void VerifySplitStringWith(string str, string[] expectedArray) => SplitStringSolution.SplitString(str).Should().Equal(expectedArray);
+}
