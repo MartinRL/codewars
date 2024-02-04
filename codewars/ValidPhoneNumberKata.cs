c51700ede65f77c2068d5df9921379f667e1c6a6
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-02-04 17:49:00 +0100

    boilerplate and tests from codewars
    https://www.codewars.com/kata/525f47c79f2f25a4db000025/

---
+++ codewars/ValidPhoneNumberKata.cs
@@ -0,0 +1,18 @@
+﻿namespace codewars;
+
+public static class Kata
+{
+    public static bool ValidPhoneNumber(string phoneNumber)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class ValidPhoneNumberTests
+{
+    [Theory]
+    [InlineData("(123) 456-7890", true)]
+    [InlineData("(1111)5X5 2345", false)]
+    public void VerifyValidPhoneNumberWith(string phoneNumber, bool expectedValidity) =>
+        Kata.ValidPhoneNumber(phoneNumber).Should().Be(expectedValidity);
+}
