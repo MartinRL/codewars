b92738a6f8d80962ee29ce1bcd37540bf31ec23d
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-02-04 16:27:29 +0100

    boilerplate and tests from codewars
    https://www.codewars.com/kata/525f50e3b73515a6db000b83/csharp

---
+++ codewars/CreatePhoneNumberKata.cs
@@ -0,0 +1,18 @@
+﻿namespace codewars;
+
+public class Kata
+{
+    public static string CreatePhoneNumber(int[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class CreatePhoneNumberTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, "(123) 456-7890")]
+    [InlineData(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "(111) 111-1111")]
+    public void VerifyCreatePhoneNumberWith(int[] numbers, string expectedPhoneNumber) => Kata.CreatePhoneNumber(numbers).Should().Be(expectedPhoneNumber);
+}
+
