903613113174228847ecf9b20605a8b4bcf086d7
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-02-04 17:23:52 +0100

    :lipstick: --> string.Format

--- codewars/CreatePhoneNumberKata.cs
+++ codewars/CreatePhoneNumberKata.cs
@@ -3,14 +3,7 @@
 public class Kata
 {
     public static string CreatePhoneNumber(int[] numbers) =>
-        numbers.Aggregate("(", (result, number) =>
-        {
-            result += number;
-            if (result.Length == 4) result += ") ";
-            else if (result.Length == 9) result += "-";
-
-            return result;
-        });
+        string.Format("{0:(000) 000-0000}", ulong.Parse(string.Join(string.Empty, numbers)));
 }
 
 public class CreatePhoneNumberTests
@@ -18,6 +11,7 @@ public class CreatePhoneNumberTests
     [Theory]
     [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, "(123) 456-7890")]
     [InlineData(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "(111) 111-1111")]
-    public void VerifyCreatePhoneNumberWith(int[] numbers, string expectedPhoneNumber) => Kata.CreatePhoneNumber(numbers).Should().Be(expectedPhoneNumber);
+    public void VerifyCreatePhoneNumberWith(int[] numbers, string expectedPhoneNumber) => 
+        Kata.CreatePhoneNumber(numbers).Should().Be(expectedPhoneNumber);
 }
 
