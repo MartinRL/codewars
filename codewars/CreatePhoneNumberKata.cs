ec2e652cb970525b0f1c1dc920a0c5857a5fc1a4
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-02-04 17:26:44 +0100

    :lipstick: simple and terse

--- codewars/CreatePhoneNumberKata.cs
+++ codewars/CreatePhoneNumberKata.cs
@@ -1,9 +1,11 @@
 ﻿namespace codewars;
 
-public class Kata
+using static System.String;
+using static System.UInt64;
+
+public class CreatePhoneNumberSolution
 {
-    public static string CreatePhoneNumber(int[] numbers) =>
-        string.Format("{0:(000) 000-0000}", ulong.Parse(string.Join(string.Empty, numbers)));
+    public static string CreatePhoneNumber(int[] numbers) => $"{Parse(Join(Empty, numbers)):(000) 000-0000}";
 }
 
 public class CreatePhoneNumberTests
@@ -11,7 +13,7 @@ public class CreatePhoneNumberTests
     [Theory]
     [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, "(123) 456-7890")]
     [InlineData(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "(111) 111-1111")]
-    public void VerifyCreatePhoneNumberWith(int[] numbers, string expectedPhoneNumber) => 
-        Kata.CreatePhoneNumber(numbers).Should().Be(expectedPhoneNumber);
+    public void VerifyCreatePhoneNumberWith(int[] numbers, string expectedPhoneNumber) =>
+        CreatePhoneNumberSolution.CreatePhoneNumber(numbers).Should().Be(expectedPhoneNumber);
 }
 
