ccb901045ca81f22bec74cf8c5a6df42ae652e73
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-02-05 07:38:28 +0100

    forgot to rename after codewars submission
    but could've had the right name to begin with since it's sufficient to copy paste only the method for this kata

--- codewars/ValidPhoneNumberKata.cs
+++ codewars/ValidPhoneNumberKata.cs
@@ -2,7 +2,7 @@
 
 using static System.Text.RegularExpressions.Regex;
 
-public static class Kata
+public static class ValidPhoneNumberSolution
 {
     public static bool ValidPhoneNumber(string phoneNumber) => IsMatch(phoneNumber, @"^\(\d{3}\) \d{3}-\d{4}$");
 }
@@ -13,5 +13,5 @@ public class ValidPhoneNumberTests
     [InlineData("(123) 456-7890", true)]
     [InlineData("(1111)5X5 2345", false)]
     public void VerifyValidPhoneNumberWith(string phoneNumber, bool expectedValidity) =>
-        Kata.ValidPhoneNumber(phoneNumber).Should().Be(expectedValidity);
+        ValidPhoneNumberSolution.ValidPhoneNumber(phoneNumber).Should().Be(expectedValidity);
 }
